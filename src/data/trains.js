729770915cf68bd5c8e6576c33b1143446b1cf91
the-darkkk <oleg7733g@gmail.com>
2026-05-10 23:09:05 +0300

    Add mockup train data

---
+++ src/data/trains.js
@@ -0,0 +1,34 @@
+export const trainsData = [
+  {
+    id: '1',
+    trainNumber: '705К',
+    route: { from: 'Київ', to: 'Львів' },
+    departureDate: '2026-05-20',
+    departureTime: '06:00',
+    duration: '05:15',
+  },
+  {
+    id: '2',
+    trainNumber: '091К',
+    route: { from: 'Київ', to: 'Львів' },
+    departureDate: '2026-05-20',
+    departureTime: '22:37',
+    duration: '07:23',
+  },
+  {
+    id: '3',
+    trainNumber: '732Ш',
+    route: { from: 'Київ', to: 'Дніпро' },
+    departureDate: '2026-05-21',
+    departureTime: '07:15',
+    duration: '06:30',
+  },
+  {
+    id: '4',
+    trainNumber: '043Л',
+    route: { from: 'Київ', to: 'Івано-Франківськ' },
+    departureDate: '2026-05-21',
+    departureTime: '20:12',
+    duration: '09:40',
+  }
+];
